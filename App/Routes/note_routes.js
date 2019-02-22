71997ed61a57846bfdc47ff9f75409219cc18e3d
Krishnateja Avvari <kavvari@uci.edu>
2019-02-22 02:55:48 -0800

    Delete and Update scheduled messages

--- App/Routes/note_routes.js
+++ App/Routes/note_routes.js
@@ -92,9 +92,30 @@ module.exports = function(app, db) {
                 }
               })
               return
+            } else if (todo == "getAllScheduledMessages") {
+              var getSchedulesData = {
+                username: req.body.username
+              }
+              db.collection('scheduled_messages').findOne(getSchedulesData, (err, item) => {
+                if (err) {
+                  res.send(err)
+                } else {
+                  res.send(item)
+                }
+              })
+              return
             }
             var getMessageData = {
               username: req.body.username
+            }
+            var updateMessageData = {
+              sender: req.body.sender,
+              receiver: req.body.receiver,
+              message: req.body.message,
+              year: req.body.year,
+              month: req.body.month,
+              day: req.body.day,
+              time: req.body.time
             }
             db.collection('scheduled_messages').findOne(getMessageData, (err, item) => {
               if (err) {
@@ -102,19 +123,67 @@ module.exports = function(app, db) {
               } else {
                 var updated = item
                 //console.log(updated)
+
                 updated._id = ObjectId(updated._id)
                 if (todo == "updateMessage") {
                   var newMessage = req.body.newMessage
-                  console.log(newMessage)
+                  //console.log(newMessage)
                   updated['listOfMessages'].push(newMessage)
-                  db.collection('scheduled_messages').update(getMessageData, updated, (err, result) => {
-                    if (err) {
-                      res.send(err);
-                    } else {
-                        res.send('Updated' + updated);
-                    }
-                  })
                 }
+                var newUpdateMessageData = {
+                  sender: req.body.newSender,
+                  receiver: req.body.newReceiver,
+                  message: req.body.newMessage,
+                  year: req.body.newYear,
+                  month: req.body.newMonth,
+                  day: req.body.newDay,
+                  time: req.body.newTime
+                }
+                var oldListOfMessages = updated['listOfMessages']
+                var startIndex = 0
+                var endIndex = oldListOfMessages.length
+                var indexOfMessage = -1
+                // oldListOfMessages.sort(function(d1, d2) {
+                //   if (JSON.parse(d1)['time'] > JSON.parse(d2)['time']) {
+                //     return 1
+                //   } else {
+                //     return -1
+                //   }
+                // })
+                // while (True) {
+                //   if newMessage['time'] < JSON.parse(oldListOfMessages[(oldListOfMessages.length/2)-1])['time'] {
+                //     endIndex = (oldListOfMessages.length/2)-1
+                //   } else if newMessage['time'] > JSON.parse(oldListOfMessages[(oldListOfMessages.length/2)-1])['time'] {
+                //     startIndex = (oldListOfMessages.length/2)-1
+                //   } else {
+                //
+                //     indexOfMessage = (oldListOfMessages.length/2)-1
+                //     break;
+                //   }
+                // }
+                // startIndex = oldListOfMessages.indexOf(JSON.stringify(newMessage))
+                // endIndex = oldListOfMessages.indexOf(JSON.stringify(newMessage))
+                console.log(JSON.stringify(updateMessageData))
+                var indexOfMessage = oldListOfMessages.indexOf(JSON.stringify(updateMessageData))
+                if (todo == "updateOldMessage") {
+                  db.collection('messages').update(updateMessageData, newUpdateMessageData, (err, newMessageResult) => {
+
+                  })
+                  console.log('supjadskfljasdkl;f')
+                  updated['listOfMessages'].splice(indexOfMessage, 1)
+                  updated['listOfMessages'].push(JSON.stringify(newUpdateMessageData))
+                } else if (todo == "deleteUpdatedMessage") {
+                  db.collection('messages').deleteOne(updateMessageData)
+                  updated['listOfMessages'].splice(indexOfMessage, 1)
+                }
+                console.log(updated)
+                db.collection('scheduled_messages').update(getMessageData, updated, (err, result) => {
+                  if (err) {
+                    res.send(err)
+                  } else {
+                    res.send('Updated' + updated)
+                  }
+                })
               }
             })
 
